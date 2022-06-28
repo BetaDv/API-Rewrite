3cdeaefec22e69c8745edbd0c76644b0cb7db33f
BetaDv <betadv.yt@gmail.com>
2022-06-28 17:18:52 +0300

    s

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
-require("./web/index")();
+// WEBSERVER
+require("./webserver/index")();
---
+++ webserver/index.js
@@ -0,0 +1,7 @@
+module.exports = () => {
+  // DEPENDENCIES
+  const express = require("express");
+
+  // EXPRESS SETUP
+  
+};
