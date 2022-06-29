11f3c78c4d6e2d7715b446c714270a4bae4763c2
BetaDv <62099784+BetaDv@users.noreply.github.com>
2022-06-29 15:38:51 +0000

    small bug fix

--- discordbot/index.js
+++ discordbot/index.js
@@ -3,7 +3,7 @@ const config = require("./data/config");
 
 const allIntents = new Intents.FLAGS.allIntents();
 
-const client = new Client({ allIntents });
+const client = new Client({ intents: new Intents(32767) });
 
 client.once("ready", () => {
   console.log("Ready!");
